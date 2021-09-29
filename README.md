4b816f468f8dd1bb9bc8587084b413ec08de9082
Vitor Hugo Salgado <vsalgadopb@gmail.com>
2021-09-29 11:43:57 -0300

    refactor: add more linters + gradle, deps and multi-module organization

--- README.md
+++ README.md
@@ -3,4 +3,10 @@
   <a href="https://github.com/vitorsalgado/test-fixture-injector/actions/workflows/ci.yml">
     <img src="https://github.com/vitorsalgado/test-fixture-injector/actions/workflows/ci.yml/badge.svg" alt="GitHub Action Status" />
   </a>
+  <a href="https://codecov.io/gh/vitorsalgado/test-fixture-injector">
+    <img src="https://codecov.io/gh/vitorsalgado/test-fixture-injector/branch/main/graph/badge.svg?token=EFC2SD81AV" alt="Codecov Badge"/>
+  </a>
+  <a href="https://conventionalcommits.org">
+    <img src="https://img.shields.io/badge/Conventional%20Commits-1.0.0-yellow.svg" alt="Conventional Commits"/>
+  </a>
 </p>
--- junit5-extension/src/test/java/org/example/FixtureParameterResolverTest.java
+++ junit5-extension/src/test/java/org/example/FixtureParameterResolverTest.java
@@ -6,21 +6,23 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 
 @ExtendWith(FixtureParameterResolver.class)
-public class FixtureParameterResolverTest {
+class FixtureParameterResolverTest {
 
   @Test
-  void test_fixture_annotation_when_fixtureTypeIsObject(
+  void itShouldInjectAnObjectInstanceWhenParameterTypeIsObject(
       final @Fixture("fixtureTest.json") TestData testData) {
     assertEquals(123, testData.getId());
     assertEquals("hello world", testData.getTest());
   }
 
   @Test
-  void should_return_helloWorldMessage(final @Fixture("fixtureTest.json") String message) {
-    assertEquals("{\n" + "  \"id\": 123,\n" + "  \"test\": \"hello world\"\n" + "}", message);
+  void itShouldInjectTheStringContentFromResourceFile(
+      final @Fixture("fixtureTest.json") String message) {
+    assertEquals("{\"id\": 123,\"test\": \"hello world\"}", message);
   }
 
   static class TestData {
+
     private int id;
     private String test;
 
